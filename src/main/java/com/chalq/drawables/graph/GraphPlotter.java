2e09d3a83a9a656b61a1a3ec3014d175e8dddaaa
Gonkee <gonkee@users.noreply.github.com>
2021-02-09 21:41:23 +1100

    graph plotter progress

--- src/main/java/com/chalq/drawables/graph/GraphPlotter.java
+++ src/main/java/com/chalq/drawables/graph/GraphPlotter.java
@@ -2,22 +2,27 @@ package com.chalq.drawables.graph;
 
 import com.chalq.core.Cq;
 import com.chalq.drawables.Drawable;
+import com.chalq.math.MathUtils;
 import com.chalq.math.Vec2;
+import com.chalq.util.Color;
 
+import java.util.ArrayList;
 import java.util.function.Function;
 
 public class GraphPlotter implements Drawable {
 
-    private final Function<Float, Vec2> parametricFunction;
+    private final ArrayList<Function<Float, Vec2>> functions = new ArrayList<>();
+    private final ArrayList<Float> minInputs = new ArrayList<>();
+    private final ArrayList<Float> maxInputs = new ArrayList<>();
+    private final ArrayList<float[]> curves = new ArrayList<>();
+    private final ArrayList<Color> curveColors = new ArrayList<>();
 
     private float x, y, width, height;
     private float xAxisMin, xAxisMax;
     private float yAxisMin, yAxisMax;
 
-    private float minInput;
-    private float maxInput;
-
-    float[] line = new float[1000];
+    private float curveWidth = 4;
+    private int levelOfDetail = 100;
 
     public static Function<Float, Vec2> toParametric(Function<Float, Float> function, boolean inverse) {
         if (inverse) {
@@ -28,8 +33,6 @@ public class GraphPlotter implements Drawable {
     }
 
     public GraphPlotter(float x, float y, float width, float height,
-                        Function<Float, Vec2> parametricFunction,
-                        float minInput, float maxInput,
                         float xAxisMin, float xAxisMax,
                         float yAxisMin, float yAxisMax )
     {
@@ -42,24 +45,37 @@ public class GraphPlotter implements Drawable {
         this.xAxisMax = xAxisMax;
         this.yAxisMin = yAxisMin;
         this.yAxisMax = yAxisMax;
-
-        this.parametricFunction = parametricFunction;
-        this.minInput = minInput;
-        this.maxInput = maxInput;
-        updateGraph();
     }
 
-    private void updateGraph() {
-        for (int i = 0; i < line.length; i += 2) {
-            Vec2 result = parametricFunction.apply(i * 0.01f);
-            line[i] = 960 + result.x * 20;
-            line[i + 1] = 500 + result.y * 20;
+    public int addFunction(Function<Float, Vec2> function, float minInput, float maxInput, Color color) {
+        int newFunctionID = functions.size();
+        functions.add(function);
+        minInputs.add(minInput);
+        maxInputs.add(maxInput);
+        curves.add(new float[levelOfDetail * 2]);
+        curveColors.add(color);
+        updateGraph(newFunctionID);
+        return newFunctionID;
+    }
+
+    public void updateAllGraphs() {
+        for (int i = 0; i < functions.size(); i++) updateGraph(i);
+    }
+
+    public void updateGraph(int index) {
+        for (int vID = 0; vID < levelOfDetail; vID++) {
+            float input = MathUtils.lerp(minInputs.get(index), maxInputs.get(index), (float) vID / levelOfDetail);
+            Vec2 result = functions.get(index).apply(input);
+            curves.get(index) [vID * 2    ] = x + (result.x - xAxisMin) / (xAxisMax - xAxisMin) * width;
+            curves.get(index) [vID * 2 + 1] = y + height - (result.y - yAxisMin) / (yAxisMax - yAxisMin) * height;
         }
     }
 
     @Override
     public void draw() {
-        updateGraph();
-        Cq.strokePolyline(line, 5);
+        for (int i = 0; i < functions.size(); i++) {
+            Cq.setColor(curveColors.get(i));
+            Cq.strokePolyline(curves.get(i), curveWidth);
+        }
     }
 }
--- src/test/java/FunctionTest.java
+++ src/test/java/FunctionTest.java
@@ -9,7 +9,7 @@ import com.chalq.util.Color;
 public class FunctionTest extends CqScene {
 
 //    FunctionGraph graph = new FunctionGraph(this::f);
-    GraphPlotter graph2 = new GraphPlotter(GraphPlotter.toParametric(this::f, false), -5, 5);
+    GraphPlotter plotter;
 
 
     public static void main(String[] args) {
@@ -17,30 +17,29 @@ public class FunctionTest extends CqScene {
         config.width = 1920;
         config.height = 1080;
         config.backgroundColor = new Color(0.102f, 0.137f, 0.2f, 1f);
+        config.antialiasing = true;
         new CqWindow(config, new FunctionTest());
     }
 
     @Override
     public void init() {
+        plotter = new GraphPlotter(200, 200, 800, 400, -10, 10, -5, 5);
+        plotter.addFunction(GraphPlotter.toParametric(this::f, false), -10, 10, new Color(1, 0, 0, 1));
+        plotter.addFunction(GraphPlotter.toParametric(this::g, false), -10, 10, new Color("#56B0FF"));
     }
 
     @Override
     public void update() {
         Cq.clearFrame();
-//        graph.draw();
-        graph2.draw();
+        plotter.draw();
     }
 
     public float f(float x) {
+        return x * x * x;
+    }
+
+    public float g(float x) {
         return (float)( Math.sin(x - Cq.time) + Math.cos(x * 0.7 + Cq.time)) * 5;
     }
 
-    public Vec2 p(float t) {
-
-        float x = (float) Math.cos(t);
-        float y = (float) Math.sin(t);
-        float scale = (float) Math.sin(t * 4) + 1;
-
-        return new Vec2(scale * x * 10, y * scale * 10);
-    }
 }
